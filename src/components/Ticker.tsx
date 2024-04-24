da862af9d337b1d55c5c9385d313c97b7da5b8a4
michaelblawrence <michael@wondering.com>
2024-04-24 12:19:01 +0100

    ticker fade in/out

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,4 +1,17 @@
 import { For, Show, createEffect, createSignal, onCleanup } from "solid-js";
+import { Transition } from "solid-transition-group";
+
+type TickerHeader = {
+  now: number;
+  size: number;
+};
+
+type TickerItem = {
+  seqIndex: number;
+  startOffsetMs: number;
+  duration: number;
+  payload: string;
+};
 
 export function Ticker({ ticker }: { ticker: () => string | null }) {
   const [now, setNow] = createSignal(Date.now());
@@ -6,42 +19,12 @@ export function Ticker({ ticker }: { ticker: () => string | null }) {
   const timer = setInterval(() => setNow(Date.now()), 1000);
   onCleanup(() => clearInterval(timer));
 
-  const splitTickerHeader = (line: string) => {
-    const [_, now, size] = line.split("\x00");
-    return { now: parseInt(now), size: parseInt(size) };
-  };
-  const splitTickerItems = (line: string) => {
-    const [header, payload] = line.split("\x00");
-    const [seqIndex, startOffsetMs, duration] = header.split("|");
-    return {
-      seqIndex: parseInt(seqIndex),
-      startOffsetMs: parseInt(startOffsetMs),
-      duration: parseInt(duration),
-      payload,
-    };
-  };
   const parsed = () => {
     const rawTicker = ticker();
-    if (!rawTicker)
-      return {
-        header: {
-          now: Date.now(),
-          size: 0,
-        },
-        items: [],
-      };
-    const [headerLine, ...lines] = rawTicker.split("\n");
-    const header = splitTickerHeader(headerLine);
-    const items = lines.map(splitTickerItems);
-    return { header, items };
+    return parseTicker(rawTicker);
   };
   const items = () => parsed().items;
-
-  createEffect(() => {
-    now();
-    const ticker = parsed();
-    console.log(ticker);
-  });
+  const sinceMs = () => now() - parsed().header.now;
 
   return (
     <div
@@ -50,21 +33,66 @@ export function Ticker({ ticker }: { ticker: () => string | null }) {
     >
       <For each={items()}>
         {({ seqIndex, startOffsetMs, duration, payload }) => (
-          <Show
-            when={
-              now() - parsed().header.now >= startOffsetMs &&
-              now() - parsed().header.now < startOffsetMs + duration
-            }
+          <Transition
+            enterActiveClass="transition-opacity duration-100"
+            enterClass="opacity-0"
+            enterToClass="opacity-100"
+            exitActiveClass="transition-opacity duration-1000"
+            exitClass="opacity-100"
+            exitToClass="opacity-0"
           >
-            <div
-              class="grid justify-start items-center bg-zinc-900/40 px-2"
-              data-index={seqIndex}
+            <Show
+              when={
+                sinceMs() >= startOffsetMs &&
+                sinceMs() < startOffsetMs + duration
+              }
             >
-              <span class="text-xl font-light text-zinc-50/60">{payload}</span>
-            </div>
-          </Show>
+              <div
+                class="grid justify-end items-center bg-zinc-900/40 px-2"
+                data-index={seqIndex}
+              >
+                <span class="text-xl font-light text-zinc-50/60">
+                  {payload}
+                </span>
+              </div>
+            </Show>
+          </Transition>
         )}
       </For>
     </div>
   );
+}
+
+function parseTicker(input: string): {
+  header: TickerHeader;
+  items: TickerItem[];
+} {
+  function splitTickerHeader(line: string) {
+    const [_, now, size] = line.split("\x00");
+    return { now: parseInt(now), size: parseInt(size) };
+  }
+  function splitTickerItems(line: string) {
+    const [header, payload] = line.split("\x00");
+    const [seqIndex, startOffsetMs, duration] = header.split("|");
+    return {
+      seqIndex: parseInt(seqIndex),
+      startOffsetMs: parseInt(startOffsetMs),
+      duration: parseInt(duration),
+      payload,
+    };
+  }
+
+  if (!input) {
+    return {
+      header: {
+        now: Date.now(),
+        size: 0,
+      },
+      items: [],
+    };
+  }
+  const [headerLine, ...lines] = input.split("\n");
+  const header = splitTickerHeader(headerLine);
+  const items = lines.map(splitTickerItems);
+  return { header, items };
 }
